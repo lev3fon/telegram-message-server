1457442684a49375fe9069d82239e7e32c6b172c
lev <levtrifonov@mail.ru>
2022-01-17 17:37:56 +0500

    Add command 'check all'

--- src/bot.js
+++ src/bot.js
@@ -6,18 +6,20 @@ const bot = new Telegraf('5016091202:AAGRRRz9PvT6a2386TNZgVYeb8OMWNnvaBs')
 
 bot.start((ctx) => ctx.reply('Драсте'))
 bot.hears('lol', (ctx) => ctx.reply('kek'))
+bot.hears('check all', async (ctx) => {
+    const ans = await checkAll(ctx.update.message.from.id)
+    ctx.reply(ans)
+})
 
 bot.on('message', async (ctx) => {
-    const ans = await messageParser(ctx.update.message.text, ctx.update.message.from.id)
+    const ans = prettyfyCheckHealthMessage(await messageParser(ctx.update.message.text, ctx.update.message.from.id))
     console.log(ans)
     ctx.reply(ans)
 })
 
 
 const messageParser = async (message, userId = undefined) => {
-    console.log(message)
     const parts = message.split(' ')
-    console.log(parts)
 
     switch (parts[0]) {
         case 'check':
@@ -31,8 +33,35 @@ const messageParser = async (message, userId = undefined) => {
     }
 }
 
-const addSite = (url) => {
+const checkAll = async (userId) => {
+    const urls = await Urls.findAll({
+        where:{
+            userId: userId
+        }
+    })
 
+    const res = []
+    for (let urlModel of urls) {
+        const statusMessage = await checkHealth(urlModel.url)
+        res.push(`${urlModel.url} ${statusMessage.status}`)
+    }
+    console.log(res)
+
+    return res.join('\n')
+}
+
+const prettyfyCheckHealthMessage = (checkMessage) =>{
+    switch (checkMessage.statusCode) {
+        case 200:
+            return 'Запрашиваемый ресурс работает'
+        case 404:
+            return 'Запрашиваемый ресурс не найден'
+        case 500:
+            return `
+            Произошла ошибка
+            error: ${checkMessage.error}
+            `
+    }
 }
 
 bot.launch()
--- src/models/check.js
+++ src/models/check.js
@@ -3,39 +3,26 @@ const superagent = require('superagent');
 const checkHealth = async (url) => {
     try {
         const response = await superagent.get(url);
-        return prettyfyCheckHealthMessage({
+        return {
             status: response.statusCode === 200 ? 'ok' : 'error',
             statusCode: response.statusCode
-        })
+        }
     } catch (error) {
         switch (error.code) {
             case 'ENOTFOUND':
-                return prettyfyCheckHealthMessage({
+                return {
                     status: 'notFound',
                     statusCode: 404
-                })
+                }
             default:
-                return prettyfyCheckHealthMessage({
+                return {
                     status: 'interError',
                     statusCode: 500,
                     error,
-                })
+                }
         }
     }
 }
 
-const prettyfyCheckHealthMessage = (checkMessage) =>{
-    switch (checkMessage.statusCode) {
-        case 200:
-            return 'Запрашиваемый ресурс работает'
-        case 404:
-            return 'Запрашиваемый ресурс не найден'
-        case 500:
-            return `
-            Произошла ошибка
-            error: ${checkMessage.error}
-            `
-    }
-}
 
 module.exports.checkHealth = checkHealth
