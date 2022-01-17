5083576040a5a9272679d59226b71deb9b5789ab
lev <levtrifonov@mail.ru>
2022-01-17 12:26:51 +0500

    implemented adding urls to db

--- src/bot.js
+++ src/bot.js
@@ -1,27 +1,38 @@
-const { Telegraf } = require('telegraf')
-const { checkHealth } = require('./models/check')
+const {Telegraf} = require('telegraf')
+const {checkHealth} = require('./models/check')
+const {Urls} = require('./db-models/index')
 
 const bot = new Telegraf('5016091202:AAGRRRz9PvT6a2386TNZgVYeb8OMWNnvaBs')
 
 bot.start((ctx) => ctx.reply('Драсте'))
 bot.hears('lol', (ctx) => ctx.reply('kek'))
-bot.hears('check')
+
 bot.on('message', async (ctx) => {
-    const ans = await messageParser(ctx.update.message.text)
+    const ans = await messageParser(ctx.update.message.text, ctx.update.message.from.id)
+    console.log(ans)
     ctx.reply(ans)
 })
 
 
-const messageParser = async (message) =>{
+const messageParser = async (message, userId = undefined) => {
     console.log(message)
     const parts = message.split(' ')
     console.log(parts)
 
-    if(parts[0] === 'check') {
-        return await checkHealth(parts[1])
-    } else {
-        return 'unknown command'
+    switch (parts[0]) {
+        case 'check':
+            return await checkHealth(parts[1])
+        case 'add': {
+            await Urls.create({userId: userId, url: parts[1]})
+            return `Ресурс ${parts[1]} добавлен`
+        }
+        default:
+            return 'unknown command'
     }
+}
+
+const addSite = (url) => {
+
 }
 
 bot.launch()
--- src/index.js
+++ src/index.js
@@ -11,5 +11,6 @@ app.listen(port, () => {
     console.log('слушаю я')
 })
 
+
 //docker run --name tg-bot -p 6432:5432 -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=post123 -e POSTGRES_DB=bot-db -d postgres
 //npx sequelize-cli model:generate --name Urls --attributes userId:int,url:text
