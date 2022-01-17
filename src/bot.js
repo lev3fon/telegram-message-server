7575d290f90dc3094ad2ee6b633739aedb942abc
lev <levtrifonov@mail.ru>
2022-01-17 21:23:31 +0500

    Add command 'list sites' and sort urls before out

--- src/bot.js
+++ src/bot.js
@@ -6,24 +6,27 @@ const bot = new Telegraf('5016091202:AAGRRRz9PvT6a2386TNZgVYeb8OMWNnvaBs')
 
 bot.start((ctx) => ctx.reply('Драсте'))
 bot.hears('lol', (ctx) => ctx.reply('kek'))
+bot.hears('list sites', async (ctx) => {
+    const ans = await getListSites(ctx.update.message.from.id)
+    ctx.reply(ans)
+})
 bot.hears('check all', async (ctx) => {
     const ans = await checkAll(ctx.update.message.from.id)
     ctx.reply(ans)
 })
 
 bot.on('message', async (ctx) => {
-    const ans = prettyfyCheckHealthMessage(await messageParser(ctx.update.message.text, ctx.update.message.from.id))
+    const ans = await messageParser(ctx.update.message.text, ctx.update.message.from.id)
     console.log(ans)
     ctx.reply(ans)
 })
-
 
 const messageParser = async (message, userId = undefined) => {
     const parts = message.split(' ')
 
     switch (parts[0]) {
         case 'check':
-            return await checkHealth(parts[1])
+            return prettyfyCheckHealthMessage(await checkHealth(parts[1]))
         case 'add': {
             await Urls.create({userId: userId, url: parts[1]})
             return `Ресурс ${parts[1]} добавлен`
@@ -45,7 +48,25 @@ const checkAll = async (userId) => {
         const statusMessage = await checkHealth(urlModel.url)
         res.push(`${urlModel.url} ${statusMessage.status}`)
     }
-    console.log(res)
+
+    res.sort()
+
+    return res.join('\n')
+}
+
+const getListSites = async (userId) => {
+    const urls = await Urls.findAll({
+        where:{
+            userId: userId
+        }
+    })
+
+    const res = []
+    for (let urlModel of urls) {
+        res.push(urlModel.url)
+    }
+
+    res.sort()
 
     return res.join('\n')
 }
