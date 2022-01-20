510b85c4dd5bbcf476f791679beaceef13380464
lev <levtrifonov@mail.ru>
2022-01-20 16:29:12 +0500

    Create http-request like bot commands

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 const express = require('express')
+const cookieParser = require('cookie-parser');
+
+const {checkUrl, listUrls, checkUrls, insertUrl, deleteUrl} = require('./models/url')
 
 const app = express()
+app.use(cookieParser())
+const host = 'localhost'
 const port = 3000
 
-app.get('/tg',(req, res) => {
+app.get('/hi', (req, res) => {
     res.send('дратуйте')
 })
 
-app.listen(port, () => {
+app.listen(port, host, () => {
     console.log('слушаю я')
 })
-
 
 //docker run --name tg-bot -p 6432:5432 -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=post123 -e POSTGRES_DB=bot-db -d postgres
 //npx sequelize-cli model:generate --name Urls --attributes userId:int,url:text
